b8b2265fb171d5c52e1acd3dfc84bb878b2052d7
Dani Bassett <67930849+danithetallgirl@users.noreply.github.com>
2020-07-23 15:07:35 -0400

    Update author.rb

--- lib/author.rb
+++ lib/author.rb
@@ -3,8 +3,6 @@ class Author
 
     attr_accessor :name, :posts
   
-    #@@post_count = 0
-    
     def initialize(name)
       @name = name
       @posts = []
@@ -13,14 +11,12 @@ class Author
     def add_post(post)
       @posts << post
       post.author = self
-      #@@post_count += 1
     end
   
     def add_post_by_title(title)
       post = Post.new(title)
       @posts << post
       post.author = self
-      #@@post_count += 1
     end
   
     def self.post_count
@@ -29,5 +25,5 @@ class Author
 
     def posts
         Post.all.select {|post| post.author == self}
-      end
+    end
   end
