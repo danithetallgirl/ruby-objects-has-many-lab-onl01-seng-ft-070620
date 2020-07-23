c16459555e3742b8a2793e80d75eaddcba32eff6
danithetallgirl <dani@danithetallgirl.com>
2020-07-23 14:57:26 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
 
     attr_accessor :name, :songs
   
-    @@song_count = 0
+    #@@song_count = 0
   
     def initialize(name)
       @name = name
@@ -12,18 +12,18 @@ class Artist
     def add_song(song)
       @songs << song
       song.artist = self
-      @@song_count += 1
+      #@@song_count += 1
     end
   
     def add_song_by_name(name)
       song = Song.new(name)
       @songs << song
       song.artist = self
-      @@song_count += 1
+      #@@song_count += 1
     end
   
     def self.song_count
-        @@song_count
+        Song.all.count
       end
 
     def songs
--- lib/author.rb
+++ lib/author.rb
@@ -3,7 +3,7 @@ class Author
 
     attr_accessor :name, :posts
   
-    @@post_count = 0
+    #@@post_count = 0
     
     def initialize(name)
       @name = name
@@ -13,18 +13,18 @@ class Author
     def add_post(post)
       @posts << post
       post.author = self
-      @@post_count += 1
+      #@@post_count += 1
     end
   
     def add_post_by_title(title)
       post = Post.new(title)
       @posts << post
       post.author = self
-      @@post_count += 1
+      #@@post_count += 1
     end
   
     def self.post_count
-      @@post_count
+      Post.all.count
     end
 
     def posts
